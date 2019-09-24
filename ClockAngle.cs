9866ee58ddf1a12cba51949352e8846ab02f9d51
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-23 21:45:30 -0700

    Return the degrees between user input hour and minute.

--- ClockAngle.cs
+++ ClockAngle.cs
@@ -15,6 +15,13 @@ class Clock
   }
   static void Main()
   {
+    Console.WriteLine("What is the hour?");
+    int hourInput = int.Parse(Console.ReadLine());
 
+    Console.WriteLine("What is the minute?");
+    int minuteInput = int.Parse(Console.ReadLine());
+
+    float result = returnDegree(hourInput, minuteInput);
+    Console.WriteLine("The degrees between the hour and minute is " + result + " degrees.");
   } 
 }
