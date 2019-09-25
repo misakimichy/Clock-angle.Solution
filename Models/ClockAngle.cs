caf00960c4423bb823b6382f14f2deedfaca8822
Student <example@example.com>
2019-09-25 10:33:37 -0700

    Use set and get to DRY.

--- Models/ClockAngle.cs
+++ Models/ClockAngle.cs
@@ -4,8 +4,8 @@ namespace ClockAngle.Models
 {
     class Clock
     {
-        public int Hour;
-        public int Minute;
+        public int Hour { get ; set; } 
+        public int Minute { get ; set; } 
 
         public Clock (int hour, int minute)
         {
